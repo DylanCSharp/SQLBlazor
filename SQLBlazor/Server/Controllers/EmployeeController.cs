d1a2ba753d2d46aa78994c4232285e6f29ec4c28
DylanCSharp <dylancox247@gmail.com>
2021-05-05 10:06:51 +0200

    Amending

--- SQLBlazor/Server/Controllers/EmployeeController.cs
+++ SQLBlazor/Server/Controllers/EmployeeController.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
 
 namespace SQLBlazor.Server.Controllers
 {
@@ -14,53 +17,122 @@ namespace SQLBlazor.Server.Controllers
     public class EmployeeController : ControllerBase
     {
         private TestingContext _context;
+        private IConfiguration _config;
 
-        public EmployeeController(TestingContext context)
+        public EmployeeController(TestingContext context, IConfiguration configuration)
         {
             _context = context;
+            _config = configuration;
         }
 
-        [HttpGet("")]
+        [HttpGet]
         public ActionResult<List<Employee>> GetEmployees()
         {
-            return _context.Employees.ToList();
+            List<Employee> employees = new();
+            using (SqlConnection conn = new(_config.GetConnectionString("BlazorDB")))
+            {
+                SqlCommand command = new("SP_GetEmployees", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Employee employee = new()
+                    {
+                        Id = (int)reader["ID"],
+                        EmpFirstname = reader["EMP_FIRSTNAME"].ToString(),
+                        EmpSecondname = reader["EMP_SECONDNAME"].ToString(),
+                        EmpEmailAddress = reader["EMP_EMAIL_ADDRESS"].ToString()
+                    };
+                    employees.Add(employee);
+                }
+                conn.Close();
+                command.Dispose();
+                reader.Close();
+            }
+            return employees;
         }
 
         [HttpGet("{id}")]
         public ActionResult<Employee> GetEmployeebyid(int id)
         {
-            return _context.Employees.FirstOrDefault(x => x.Id == id);
+            Employee employee = new();
+
+            using (SqlConnection conn = new(_config.GetConnectionString("BlazorDB")))
+            {
+                SqlCommand command = new("SP_GetEmployeeByID", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.AddWithValue("@ID", id);
+                conn.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    employee.Id = (int)reader["ID"];
+                    employee.EmpFirstname = reader["EMP_FIRSTNAME"].ToString();
+                    employee.EmpSecondname = reader["EMP_SECONDNAME"].ToString();
+                    employee.EmpEmailAddress = reader["EMP_EMAIL_ADDRESS"].ToString();
+                }
+            }
+            return employee;
         }
 
-        [HttpPost("")]
+        [HttpPost]
         public Employee ReturnEmployee(Employee employee)
         {
-            _context.Employees.Add(employee);
-            _context.SaveChanges();
+            using (SqlConnection conn = new(_config.GetConnectionString("BlazorDB")))
+            {
+                SqlCommand command = new("SP_AddEmployee", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@FIRSTNAME", employee.EmpFirstname);
+                command.Parameters.AddWithValue("@SECONDNAME", employee.EmpSecondname);
+                command.Parameters.AddWithValue("@EMAIL", employee.EmpEmailAddress);
+
+                conn.Open();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
             return employee;
         }
 
         [HttpPut("{id}")]
         public Employee PutEmployee(int id, Employee newEmployee)
         {
-            Employee oldEmployee = _context.Employees.FirstOrDefault(x => x.Id == id);
-            oldEmployee.EmpFirstname = newEmployee.EmpFirstname;
-            oldEmployee.EmpSecondname = newEmployee.EmpSecondname;
-            oldEmployee.EmpEmailAddress = newEmployee.EmpEmailAddress;
-
-            _context.SaveChanges();
-
-            return oldEmployee;
+            using (SqlConnection conn = new(_config.GetConnectionString("BlazorDB")))
+            {
+                newEmployee.Id = id;
+                SqlCommand command = new("SP_UpdateEmployee", conn);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@ID", newEmployee.Id);
+                command.Parameters.AddWithValue("@FIRST_NAME", newEmployee.EmpFirstname);
+                command.Parameters.AddWithValue("@SECOND_NAME", newEmployee.EmpSecondname);
+                command.Parameters.AddWithValue("@EMAIL", newEmployee.EmpEmailAddress);
+                conn.Open();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            return newEmployee;
         }
 
-        [HttpDelete("")]
+        [HttpDelete]
         public int DeleteEmployee(int id)
         {
-            Employee oldEmployee = _context.Employees.FirstOrDefault(x => x.Id == id);
-            _context.Employees.Remove(oldEmployee);
-
-            _context.SaveChanges();
-
+            using (SqlConnection conn = new(_config.GetConnectionString("BlazorDB")))
+            {
+                SqlCommand command = new("SP_DeleteEmployee", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
             return id;
         }
     }
